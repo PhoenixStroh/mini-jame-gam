495a22f01bdd57556e9c31bb8559a5879e72f5e2
anthonygfong <anthonygfong@gmail.com>
2026-02-07 22:00:56 -0500

    Updated fish implementation to prepare for splash screen.

--- mini-jame-gam-51/actors/fish/fish.gd
+++ mini-jame-gam-51/actors/fish/fish.gd
@@ -1,9 +1,16 @@
 class_name Fish
-extends Sprite2D
+extends Node2D
 
 @export var start_point: Node2D
 @export var end_point: Node2D
+@export var catch_box: CollisionShape2D
+
 @export var movement_duration: float = 1.0
+
+@export var species_name: String
+@export var species_picture: Texture2D
+
+@export var _area: Area2D
 
 var _start_position: Vector2
 var _end_position: Vector2
@@ -14,6 +21,9 @@ func _ready() -> void:
 	_start_position = start_point.global_position
 	_end_position = end_point.global_position
 	
+	# Reparent the collision shape so you can manually set it in the game scene.
+	catch_box.reparent(_area)
+
 	# Move the fish.
 	_movement_tween = get_tree().create_tween().set_loops()
 	_movement_tween.tween_property(self, "global_position", _start_position, movement_duration).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
--- mini-jame-gam-51/game.gd
+++ mini-jame-gam-51/game.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
 @export var label: Label
+@export var fish_picture: TextureRect
 
 
 func _on_hook_caught_fish(fish: Fish) -> void:
 	label.visible = true
+	fish_picture.visible = true
+	
+	label.text = "You caught a " + fish.species_name + "!"
+	fish_picture.texture = fish.species_picture
 	await get_tree().create_timer(2).timeout
+
 	label.visible = false
+	fish_picture.visible = false
