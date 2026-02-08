85a1a13e24bcff08c8a785ddcc64902c1613cca4
Phoenix Stroh <phoenixstroh@gmail.com>
2026-02-08 17:07:19 -0500

    change audio manager, add new main

--- mini-jame-gam-51/audio_manager.gd
+++ mini-jame-gam-51/audio_manager.gd
@@ -8,7 +8,7 @@ func on_changed_fishing(is_fishing: bool):
     var under_volume := 0 if is_fishing else -99
 
     AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Overwater"), over_volume)
-    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Overwater"), under_volume)
+    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Underwater"), under_volume)
 
 func on_during_fish_splash(fish: Fish):
     add_fish(fish)
@@ -17,6 +17,6 @@ func add_fish(fish: Fish):
     print("HERE???")
     var instance : AudioStreamPlayer = overwater_player.instantiate()
     add_child(instance)
-    
+
     instance.stream = fish._audio.stream
     instance.play(fish._audio.get_playback_position())
