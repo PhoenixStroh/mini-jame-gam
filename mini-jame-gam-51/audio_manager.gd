48e08006000d963897f44b4b0cf33602805de5c7
Phoenix Stroh <phoenixstroh@gmail.com>
2026-02-08 16:46:25 -0500

    add overwater audio and disable caught fish

--- mini-jame-gam-51/audio_manager.gd
+++ mini-jame-gam-51/audio_manager.gd
@@ -3,8 +3,20 @@ extends Node
 
 @export var overwater_player: PackedScene
 
-func add_fish(fish: Fish):
-    var instance : AudioStreamPlayer3D = overwater_player.instantiate()
-    instance.stream = fish._audio.stream
+func on_changed_fishing(is_fishing: bool):
+    var over_volume := -999 if is_fishing else 0
+    var under_volume := 0 if is_fishing else -99
 
+    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Overwater"), over_volume)
+    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Overwater"), under_volume)
+
+func on_during_fish_splash(fish: Fish):
+    add_fish(fish)
+
+func add_fish(fish: Fish):
+    print("HERE???")
+    var instance : AudioStreamPlayer = overwater_player.instantiate()
     add_child(instance)
+    
+    instance.stream = fish._audio.stream
+    instance.play(fish._audio.get_playback_position())
